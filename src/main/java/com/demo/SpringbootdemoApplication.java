5b765787fea9be27d1c51827218c2e2c12c95d99
grant lee <grant@sina.cn>
2018-04-21 12:14:08 +0800

    init

---
+++ src/main/java/com/demo/SpringbootdemoApplication.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 暂不读取数据源配置
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class SpringbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoApplication.class, args);
+	}
+}
